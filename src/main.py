e490604d339c754408a2528a58344c2aaea03990
epen3420 <epen3420@gmail.com>
2025-10-19 01:54:26 +0900

    feat: 締め切りを過ぎた、進行中のタスクを表示する

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import notion_client as ntn
 import discord_client as dc
 import utils
 import config
+from datetime import datetime, timezone
 
 def main():
     member_dict = ntn.retrieve_member_dict()
@@ -14,20 +15,28 @@ def main():
 
     task_count = 0
     conference_count = 0
+
+    notified_task_progress = []
     for record in data.get(config.KEY_RESULTS, []):
         page_id = record.get(config.KEY_ID)
 
         if not page_id:
             continue
 
-        if page_id in notified_task_state:
-            continue
-
         title = ntn.get_title(record)
         if not title:
             continue
 
-        datetime = ntn.get_deadline(record)
+
+        time_str = ntn.get_deadline(record)
+        dt_obj = datetime.fromisoformat(time_str)
+
+        if datetime.now(timezone.utc) >= dt_obj.replace(tzinfo=timezone.utc):
+            notified_task_progress.append([title, page_id])
+            continue
+
+        if page_id in notified_task_state:
+            continue
 
         assignee_records = ntn.get_relation_records(record, config.PROP_ASSIGNEE_NAME)
         assignee_names = [member_dict[assignee_id.get(config.KEY_ID)] for assignee_id in assignee_records]
@@ -35,10 +44,10 @@ def main():
         is_task = ntn.get_select(record, config.PROP_KIND_NAME)
 
         if is_task == config.PROP_TASK_NAME:
-            dc.send_task_message(title, page_id, assignee_names, datetime)
+            dc.send_task_message(title, page_id, assignee_names, time_str)
             task_count = task_count + 1
         elif is_task == config.PROP_CONFERENCE_NAME:
-            dc.send_conference_message(title, page_id, assignee_names, datetime)
+            dc.send_conference_message(title, page_id, assignee_names, time_str)
             conference_count = conference_count + 1
         else:
             continue
@@ -46,6 +55,14 @@ def main():
         notified_task_state.setdefault(page_id, title)
 
     utils.save_notified_task_state(notified_task_state)
+
+    if len(notified_task_progress) != 0:
+        print(f"締め切りを過ぎて、進行中のタスクが{len(notified_task_progress)}件あります")
+        print("以下のタスクが完了している場合は、更新してください")
+        for task in notified_task_progress:
+            print(f"  - {task[0]}\n\t{ntn.build_notion_url(task[1])}")
+        print()
+
 
     if task_count + conference_count <= 0:
         print("未通知のタスクまたは会議はありませんでした")
